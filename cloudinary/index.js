f7e872916b1af8c7e98ff667ecb213450ab1d780
RaidenOM <omkumars2005@gmail.com>
2025-02-02 01:28:35 +0530

    updated

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -1,4 +1,3 @@
-const multer = require("multer");
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
@@ -8,31 +7,13 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-// Updated Cloudinary Storage configuration
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
-  params: (req, file) => {
-    const fileType = file.mimetype.split("/")[0];
-
-    // Check if file type is an image or video
-    if (fileType === "image") {
-      return {
-        folder: "Sierra/images",
-        allowedFormats: ["jpeg", "png", "jpg"],
-      };
-    } else if (fileType === "video") {
-      return {
-        folder: "Sierra/videos",
-        allowedFormats: ["mp4", "mov"],
-      };
-    }
-
-    throw new Error("Invalid file type");
+  params: {
+    folder: "Sierra",
+    allowed_formats: ["jpeg", "png", "jpg", "mp4", "mp3", "mov"],
   },
 });
-
-// Use the new storage configuration with multer
-const upload = multer({ storage: storage });
 
 module.exports = {
   cloudinary,
