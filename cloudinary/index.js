3729effb28af334e94d1d540ad20afccaace6871
RaidenOM <omkumars2005@gmail.com>
2025-02-05 21:30:01 +0530

    updated

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -10,12 +10,22 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: (req, file) => {
-    // Determine if the file is a video or image
     const isVideo = file.mimetype.startsWith("video/");
+    const isAudio = file.mimetype.startsWith("audio/");
+
     return {
       folder: "Sierra",
-      allowed_formats: ["jpeg", "png", "jpg", "mp4", "mov"], // Use underscores for allowed_formats
-      resource_type: isVideo ? "video" : "image", // Set resource_type dynamically
+      allowed_formats: [
+        "jpeg",
+        "png",
+        "jpg",
+        "mp4",
+        "mov",
+        "mp3",
+        "wav",
+        "ogg",
+      ],
+      resource_type: isVideo ? "video" : isAudio ? "raw" : "image", // Audio files should be "raw"
     };
   },
 });
