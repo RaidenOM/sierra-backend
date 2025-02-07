5404de96b199afc4f555f1b963bc2befe4835288
RaidenOM <omkumars2005@gmail.com>
2025-02-07 22:39:08 +0530

    updated

--- app.js
+++ app.js
@@ -114,16 +114,15 @@ app.put(
     const { bio, unsetProfilePhoto } = req.body;
     const profilePhoto = req.file ? req.file.path : null;
 
-    const updatedUser = await User.findByIdAndUpdate(
-      id,
-      {
-        bio: bio,
-        profilePhoto: unsetProfilePhoto
-          ? "https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o="
-          : profilePhoto,
-      },
-      { new: true }
-    );
+    const updatedFields = { bio };
+
+    if (unsetProfilePhoto) {
+      updatedFields.profilePhoto =
+        "https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o=";
+    } else if (profilePhoto) {
+      updatedFields.profilePhoto = profilePhoto;
+    }
+
     res.json(updatedUser);
   })
 );
