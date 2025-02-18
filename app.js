2613a3d3b92c4b1ac1a7c5b0ea6d9e24318c23d0
RaidenOM <omkumars2005@gmail.com>
2025-02-18 22:02:03 +0530

    updated

--- app.js
+++ app.js
@@ -118,8 +118,7 @@ app.put(
     const updatedFields = { bio: bio || "Hey there, I am using Sierra!" };
 
     if (unsetProfilePhoto) {
-      updatedFields.profilePhoto =
-        "https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o=";
+      updatedFields.profilePhoto = null;
     } else if (profilePhoto) {
       updatedFields.profilePhoto = profilePhoto;
     }
